de90f4909b814b1b0fe04c5bd229103fa26579df
Blackstar-coding <connor.j.hutchinson16@gmail.com>
2026-04-09 20:51:00 -0500

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -4,22 +4,24 @@ from television import *
 class Test:
     def setup_method(self):
         self.t1 = Television()
-        self.t1.power()
+        
+    def teardown_method(self):
+        del self.t1
         
     def test_init(self):
-        # Since every other test needs the power on I have to manually turn it back off for the test
-        self.t1.power()
         assert self.t1._Television__status == False
         assert self.t1._Television__muted == False
         assert self.t1._Television__volume == 0
         assert self.t1._Television__channel == 0
         
     def test_power(self):
+        self.t1.power()
         assert self.t1._Television__status == True
         self.t1.power()
         assert self.t1._Television__status == False
         
     def test_mute(self):
+        self.t1.power()
         assert self.t1._Television__muted == False
         self.t1.mute()
         assert self.t1._Television__muted == True
@@ -27,6 +29,7 @@ class Test:
         assert self.t1._Television__muted == False
         
     def test_channel_up(self):
+        self.t1.power()
         assert self.t1._Television__channel == 0
         self.t1.channel_up()
         assert self.t1._Television__channel == 1
@@ -38,6 +41,7 @@ class Test:
         assert self.t1._Television__channel == 0
         
     def test_channel_down(self):
+        self.t1.power()
         assert self.t1._Television__channel == 0
         self.t1.channel_down()
         assert self.t1._Television__channel == 3
@@ -49,6 +53,7 @@ class Test:
         assert self.t1._Television__channel == 0
         
     def test_volume_up(self):
+        self.t1.power()
         assert self.t1._Television__volume == 0
         self.t1.volume_up()
         assert self.t1._Television__volume == 1
@@ -58,6 +63,7 @@ class Test:
         assert self.t1._Television__volume == 2
         
     def test_volume_down(self):
+        self.t1.power()
         assert self.t1._Television__volume == 0
         self.t1.volume_down()
         assert self.t1._Television__volume == 0
